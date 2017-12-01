97f71577f62bab7b10a70539a3208012f9b34668
PritamDsouza <30990020+PritamDsouza@users.noreply.github.com>
2017-11-30 20:54:41 -0600

    Initial commit

---
+++ Kitchen_Mahal/public/javascripts/index.js
@@ -0,0 +1,433 @@
+﻿var module = angular.module("KitchenMahal", ["ui.router", "ngResource"]);
+
+module.config(
+    function ($stateProvider, $urlRouterProvider, $locationProvider) {
+        $urlRouterProvider.otherwise("/Home");
+        $stateProvider.state(
+            "Home", {
+                url: "/Home",
+                views: {
+                    main: {
+                        templateUrl: "pages/home.html"
+                    }
+                }
+            }
+        );
+        $stateProvider.state(
+            "Aboutus", {
+                url: "/Aboutus",
+                views: {
+                    main: {
+                        templateUrl: "pages/aboutus.html"
+                    }
+                }
+            }
+        );
+        $stateProvider.state(
+            "Menu", {
+                url: "/Menu",
+                views: {
+                    main: {
+                        templateUrl: "pages/menu.html",
+                    }
+                }
+            }
+        );
+        $stateProvider.state(
+            "TableReservation", {
+                url: "/TableReservation",
+                views: {
+                    main: {
+                        templateUrl: "pages/tableReservation.html"
+                    }
+                }
+            }
+        );
+        $stateProvider.state(
+            "Contactus", {
+                url: "/Contactus",
+                views: {
+                    main: {
+                        templateUrl: "pages/contactus.html"
+                    }
+                }
+            }
+        );
+        $stateProvider.state(
+            "Drinks", {
+                url: "/Drinks",
+                views: {
+                    main: {
+                        templateUrl: "pages/drinks.html"
+                    }
+                }
+            }
+        );
+        $stateProvider.state(
+            "Appetizer", {
+                url: "/Appetizer",
+                views: {
+                    main: {
+                        templateUrl: "pages/appetizer.html"
+                    }
+                }
+            }
+        );
+        $stateProvider.state(
+            "Entrees", {
+                url: "/Entrees",
+                views: {
+                    main: {
+                        templateUrl: "pages/entrees.html"
+                    }
+                }
+            }
+        );
+        $stateProvider.state(
+            "Deserts", {
+                url: "/Deserts",
+                views: {
+                    main: {
+                        templateUrl: "pages/deserts.html"
+                    }
+                }
+            }
+        );
+        $stateProvider.state(
+            "username", {
+                url: "/username",
+                views: {
+                    loginAs: {
+                        templateUrl: "pages/username.html"
+                    }
+                }
+            }
+        );
+        $stateProvider.state(
+            "SignUp", {
+                url: "/SignUp",
+                views: {
+                    main: {
+                        templateUrl: "pages/signup.html"
+                    }
+                }
+            }
+        );
+        $stateProvider.state(
+            "Logout", {
+                url: "/Logout",
+                views: {
+                    main: {
+                        templateUrl: "pages/home.html"
+                    }
+                }
+            }
+        );
+        $stateProvider.state(
+            "Orders", {
+                url: "/Orders",
+                views: {
+                    main: {
+                        templateUrl: "pages/orders.html"
+                    }
+                }
+            }
+        );
+        $stateProvider.state(
+            "Checkout", {
+                url: "/Checkout",
+                views: {
+                    main: {
+                        templateUrl: "pages/checkoutPage.html"
+                    }
+                }
+            }
+        );
+        $stateProvider.state(
+            "OrderHistory", {
+                url: "/OrderHistory",
+                views: {
+                    main: {
+                        templateUrl: "pages/orderHistory.html"
+                    }
+                }
+            }
+        );
+    }
+);
+
+module.controller("addCustomers", function ($scope, $resource) {
+    $scope.save = function () {
+        var request = $resource("/users/add");
+        request.save(
+            {
+                firstName: $scope.FirstName,
+                lastName: $scope.LastName,
+                emailAddress: $scope.EmailAddress,
+                contactNo: $scope.ContactNo,
+                address: $scope.Address,
+                zipcode: $scope.ZipCode,
+                state: $scope.State,
+                username: $scope.UserName,
+                password: $scope.Password
+            }, function (response) {
+                if (response.error) {
+                    $scope.error = response.error;
+                } else {
+                    $scope.message = "Customer has been successfully added.";
+                    $scope.FirstName = "";
+                    $scope.LastName = "";
+                    $scope.EmailAddress = "";
+                    $scope.ContactNo = "";
+                    $scope.Address = "";
+                    $scope.ZipCode = "";
+                    $scope.State = "";
+                    $scope.UserName = "";
+                    $scope.Password = "";
+                }
+            });
+    }
+});
+
+module.controller("drinksItems", function ($scope, $resource) {
+    var request = $resource("/users/drinks");
+    request.query(function (data) {
+        $scope.items = data;
+    });
+    $scope.addToCart = function (item, quantity){
+        var request = $resource("/users/orders");
+        if (quantity > 0) {
+            quantity = quantity;
+        }
+        else {
+            quantity = 1;
+        }
+        var totalPrice = Math.round((quantity * item.price)*100) / 100;
+        request.save(
+            {
+                foodId: item.drinkId,
+                foodName: item.drinkName,
+                imageUrl: item.imageURL,
+                price: item.price,
+                quantity: quantity,
+                totalPrice: totalPrice
+            }, function (response) {
+                alert(item.drinkName + ": Added to Orders");
+            });
+        
+    }
+});
+
+module.controller("appetizerItems", function ($scope, $resource) {
+    var request = $resource("/users/appetizer");
+    request.query(function (data) {
+        $scope.items = data;
+    });
+    $scope.addToCart = function (item, quantity){
+        var request = $resource("/users/orders");
+        if (quantity > 0) {
+            quantity = quantity;
+        }
+        else {
+            quantity = 1;
+        }
+        var totalPrice = Math.round((quantity * item.price) * 100) / 100;
+        request.save(
+            {
+                foodId: item.appetizerId,
+                foodName: item.appetizerName,
+                imageUrl: item.imageUrl,
+                price: item.price,
+                quantity: quantity,
+                totalPrice: totalPrice
+            }, function (response) {
+                alert(item.appetizerName + ": Added to Orders");
+            });
+        
+    }
+});
+
+module.controller("entreesItems", function ($scope, $resource) {
+    var request = $resource("/users/entrees");
+    request.query(function (data) {
+        $scope.items = data;
+    });
+    $scope.addToCart = function (item, quantity){
+        var request = $resource("/users/orders");
+        if (quantity > 0) {
+            quantity = quantity;
+        }
+        else {
+            quantity = 1;
+        }
+        var totalPrice = Math.round((quantity * item.price) * 100) / 100;
+        request.save(
+            {
+                foodId: item.entreeId,
+                foodName: item.entreeName,
+                imageUrl: item.imageUrl,
+                price: item.price,
+                quantity: quantity,
+                totalPrice: totalPrice
+            }, function (response) {
+                alert(item.entreeName + ": Added to Orders");
+            });
+    }
+});
+
+module.controller("desertsItems", function ($scope, $resource) {
+    var request = $resource("/users/deserts");
+    request.query(function (data) {
+        $scope.items = data;
+    });
+    $scope.addToCart = function (item, quantity){
+        var request = $resource("/users/orders");
+        if (quantity > 0) {
+            quantity = quantity;
+        }
+        else {
+            quantity = 1;
+        }
+        var totalPrice = Math.round((quantity * item.price) * 100) / 100;
+        request.save(
+            {
+                foodId: item.desertId,
+                foodName: item.desertName,
+                imageUrl: item.imageUrl,
+                price: item.price,
+                quantity: quantity,
+                totalPrice: totalPrice
+            }, function (response) {
+                alert(item.desertName + ": Added to Orders");
+            });
+    }
+});
+
+module.controller("tableReservation", function ($scope, $resource) {
+    $scope.checkAvailability = function () {
+        var request = $resource("/users/tablereservation");
+        request.save(
+            {
+                people: $scope.People,
+                date: ((($scope.BookingDate).getMonth() + 1) + "/" + ($scope.BookingDate).getDate() + "/" + ($scope.BookingDate).getFullYear()),
+                time: $scope.BookingTime
+            }, function (response) {
+                if (response.error == "Not Available") {
+                    $scope.message = "Not Available, Please alter the selection!!!";
+                } else {
+                    $scope.message = "Congratulations!!! Table has been successfully booked.";
+                }
+            });
+    }
+});
+
+module.controller("contactUs", function ($scope, $resource) {
+    $scope.saveMessage = function () {
+        var request = $resource("/users/contactus");
+        request.save(
+            {
+                name: $scope.Name,
+                email: $scope.Email,
+                message: $scope.Message
+            }, function (response) {
+                if (response.error) {
+                     response.error;
+                } else {
+                    $scope.message = "Message Successfully Sent.";
+                    $scope.Name = "";
+                    $scope.Email = "";
+                    $scope.Message = "";
+                }
+            });
+    }
+});
+
+module.controller("loginCustomer", function ($scope, $http, $resource) {
+    $http.get("/users/username").then(function(data){
+        if(data.data != "Not logged on"){
+            $scope.username = data.data;
+            $scope.currentUserLoggedIn = true;
+        } else{
+            $scope.currentUserLoggedIn = false;
+        }
+    });
+    $scope.signin = function () {
+        var request = $resource("/users/signin");
+        request.save(
+            {
+                username: $scope.UserName,
+                password: $scope.Password
+            }, function (response) {
+                if (response.error) {
+                    $scope.message = response.error;
+                } else {
+                    $scope.currentUserLoggedIn = true;
+                    $scope.username = response.username;
+                    $scope.UserName = "";
+                    $scope.Password = "";
+                    location.reload();
+                }
+            });
+    }
+    $scope.logout = function () {
+        $http.get("/users/logout").then(function(data){
+         $scope.message = data.data;
+        location.reload();
+    });
+    }
+});
+
+module.controller("cart", function ($scope, $resource) {
+    var request = $resource("/users/cart");
+    request.query(function (data) {
+        $scope.items = data;
+    });
+    $scope.removeItem = function (foodId) {
+        var request = $resource("/users/deletefromcart");
+        request.save(
+            {
+                foodId: foodId
+            }, function (response) {
+                $scope.message = response;
+                location.reload();
+            });
+    }
+    $scope.checkout = function (items) {
+        var request = $resource("/users/checkout");
+        var totalAmount = 0;
+        for(var i = 0; i < items.length;i++){
+            totalAmount += +(items[i].totalPrice);
+        }
+        totalAmount = Math.round(totalAmount * 100) / 100;
+        request.save(
+            {
+                totalAmount : totalAmount
+            }, function (response) {
+                if (response.error) {
+                    $scope.message = response.error;
+                    $scope.showCheckoutPage = false;
+                    alert($scope.message);
+                } else {
+                    $scope.message = response.success;
+                    $scope.showCheckoutPage = true;
+                    $scope.totalAmount = totalAmount;
+                }
+            });
+    }
+    $scope.DisplayPaymentMessage = function () {
+        var request = $resource("/users/onPaymentRemoveFromCart");
+        request.save(
+            {
+            }, function (response) {
+                location.reload();
+                alert("Payment Done!!!Enjoy Your Food");
+            });
+    }
+});
+
+module.controller("OrderHistory", function ($scope, $resource) {
+    var request = $resource("/users/orderHistory");
+    request.query(function (data) {
+       $scope.items = data;
+    });
+});
---
+++ Kitchen_Mahal/routes/users.js
@@ -0,0 +1,285 @@
+﻿var express = require('express');
+var router = express.Router();
+var fs = require("fs");
+var session;
+/* GET users listing. */
+router.get('/', function (req, res) {
+    res.send('respond with a resource');
+});
+
+/* GET customers listing. */
+router.get('/', function (req, res) {
+    var config = require('config');
+    var json = fs.readFileSync(config.dataPath + '/customers.json', 'utf8').trim();
+    var customers = JSON.parse(json);
+    res.end(JSON.stringify(customers));
+});
+
+router.post("/add", function (req, res) {
+    var config = require('config');
+    var customers = loadCustomers();
+    var customer = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        emailAddress: req.body.emailAddress,
+        contactNo: req.body.contactNo,
+        address: req.body.address,
+        zipcode: req.body.zipcode,
+        state: req.body.state,
+        username: req.body.username,
+        password: req.body.password
+    };
+    customers.push(customer);
+    fs.writeFileSync(config.dataPath + '/customers.json', JSON.stringify(customers), "utf8");
+    var response = {
+        customer: customer,
+        error: ""
+    };
+    res.end(JSON.stringify(response));
+});
+
+function loadCustomers() {
+    var config = require('config');
+    var json = fs.readFileSync(config.dataPath + '/customers.json', 'utf8').trim();
+    var customers = JSON.parse(json);
+    return customers;
+}
+
+/* GET drinks listing. */
+router.get('/drinks', function (req, res) {
+    var config = require('config');
+    var json = fs.readFileSync(config.dataPath + '/drinks.json', 'utf8').trim();
+    var drinks = JSON.parse(json);
+    res.end(JSON.stringify(drinks));
+});
+
+/* GET appetizer listing. */
+router.get('/appetizer', function (req, res) {
+    var config = require('config');
+    var json = fs.readFileSync(config.dataPath + '/appetizer.json', 'utf8').trim();
+    var appetizer = JSON.parse(json);
+    res.end(JSON.stringify(appetizer));
+});
+
+/* GET entrees listing. */
+router.get('/entrees', function (req, res) {
+    var config = require('config');
+    var json = fs.readFileSync(config.dataPath + '/entrees.json', 'utf8').trim();
+    var entrees = JSON.parse(json);
+    res.end(JSON.stringify(entrees));
+});
+
+/* GET deserts listing. */
+router.get('/deserts', function (req, res) {
+    var config = require('config');
+    var json = fs.readFileSync(config.dataPath + '/deserts.json', 'utf8').trim();
+    var deserts = JSON.parse(json);
+    res.end(JSON.stringify(deserts));
+});
+
+router.post('/tablereservation', function (req, res) {
+    var config = require('config');
+    var json = fs.readFileSync(config.dataPath + '/tablereservation.json', 'utf8').trim();
+    var tableReservations = JSON.parse(json);
+    var availability = false;
+    var response;
+    for (var i = 0; i < tableReservations.length; i++) {
+        if (!(tableReservations[i].date == req.body.date && tableReservations[i].time == req.body.time)) {
+            availability = true;
+        } else {
+            availability = false;
+            response = {
+                error: "Not Available",
+            };
+            break;
+        }
+    }
+    if (availability == true) {
+        var newReservation = {
+            people: req.body.people,
+            date: req.body.date,
+            time: req.body.time
+        };
+        tableReservations.push(newReservation);
+        fs.writeFileSync(config.dataPath + '/tablereservation.json', JSON.stringify(tableReservations), "utf8");
+        response = {
+            newReservation: newReservation,
+        };
+    }
+    res.end(JSON.stringify(response));
+});
+
+router.post('/contactus', function (req, res) {
+    var config = require('config');
+    var json = fs.readFileSync(config.dataPath + '/contactus.json', 'utf8').trim();
+    var userMessages = JSON.parse(json);
+    var userMessage = {
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message,
+    };
+    userMessages.push(userMessage);
+    fs.writeFileSync(config.dataPath + '/contactus.json', JSON.stringify(userMessages), "utf8");
+    var response = {
+        userMessage: userMessage,
+        error: ""
+    };
+    res.end(JSON.stringify(response));
+});
+
+router.post('/signin', function (req, res) {
+    var config = require('config');
+    var json = fs.readFileSync(config.dataPath + '/customers.json', 'utf8').trim();
+    var customers = JSON.parse(json);
+    var status = false;
+    var response;
+    for (var i = 0; i < customers.length; i++) {
+        if (req.body.username == customers[i].username && req.body.password == customers[i].password) {
+            status = true;
+            break;
+        } else {
+            status = false;
+            response = {
+                error: "Username and password incorrect"
+            };
+        }
+    }
+
+    if (status == true) {
+        session = req.session;
+        session.loginAs = req.body.username;
+        session.save();
+        response = {
+            username: req.session.loginAs ? req.session.loginAs : "Not logged on"
+        };
+    }
+    res.end(JSON.stringify(response));
+});
+
+/* GET username. */
+router.get('/username', function (req, res) {
+    res.send(req.session.loginAs ? req.session.loginAs : "Not logged on");
+});
+
+router.get('/logout', function (req, res) {
+    delete req.session.loginAs;
+    res.end("logged out");
+});
+
+router.post('/orders', function (req, res) {
+    var config = require('config');
+    var json = fs.readFileSync(config.dataPath + '/orders.json', 'utf8').trim();
+    var orders = JSON.parse(json);
+    var order = {
+        username : req.session.loginAs,
+        foodId: req.body.foodId,
+        foodName: req.body.foodName,
+        imageUrl: req.body.imageUrl,
+        price: req.body.price,
+        quantity: req.body.quantity,
+        totalPrice: req.body.totalPrice,
+    };
+    orders.push(order);
+    fs.writeFileSync(config.dataPath + '/orders.json', JSON.stringify(orders), "utf8");
+    var response = {
+        order: order,
+        error: ""
+    };
+    res.end(JSON.stringify(response));
+});
+
+/* GET cart listing. */
+router.get('/cart', function (req, res) {
+    var config = require('config');
+    var json = fs.readFileSync(config.dataPath + '/orders.json', 'utf8').trim();
+    var orders = JSON.parse(json);
+    var cartOrders = [];
+    for(var i = 0; i < orders.length; i++){
+        if(orders[i].username == req.session.loginAs){
+            cartOrders.push(orders[i]);
+        } 
+    }
+    res.end(JSON.stringify(cartOrders));
+});
+
+router.post('/deletefromcart', function (req, res) {
+    var config = require('config');
+    var json = fs.readFileSync(config.dataPath + '/orders.json', 'utf8').trim();
+    var orders = JSON.parse(json);
+    for(var i = 0; i < orders.length; i++){
+        if (orders[i].foodId == req.body.foodId) {
+            orders.splice(i, 1);
+        }
+    }
+    fs.writeFileSync(config.dataPath + '/orders.json', JSON.stringify(orders), "utf8");
+    res.end("Record deleted");
+});
+
+router.post('/onPaymentRemoveFromCart', function (req, res) {
+    var config = require('config');
+    var json = fs.readFileSync(config.dataPath + '/orders.json', 'utf8').trim();
+    var orders = JSON.parse(json);
+    for (var i = 0; i < orders.length; i++) {
+        if (orders[i].username == req.session.loginAs) {
+            orders.splice(i);
+        }
+    }
+    fs.writeFileSync(config.dataPath + '/orders.json', JSON.stringify(orders), "utf8");
+    res.end("Payment Done!!!Enjoy Your Food");
+});
+
+router.post('/checkout', function (req, res) {
+    
+    var config = require('config');
+    var json = fs.readFileSync(config.dataPath + '/customers.json', 'utf8').trim();
+    var customers = JSON.parse(json);
+    var json2 = fs.readFileSync(config.dataPath + '/orderHistory.json', 'utf8').trim();
+    var orderHistorys = JSON.parse(json2);
+    var Email = "";
+    var Address = "";
+    var response;
+    var OrderId = Math.floor((Math.random()*50) + 1);
+    var OrderDate = new Date();
+    if (req.session.loginAs) {
+        for (var i = 0; i < customers.length; i++) {
+            if (customers[i].username == req.session.loginAs) {
+                Email = customers[i].emailAddress;
+                Address = customers[i].address + ", " + customers[i].state + "-" + customers[i].zipcode;
+            }
+        }
+        var orderHistory = {
+            orderId: OrderId,
+            orderDate: OrderDate,
+            username: req.session.loginAs,
+            emailAddress: Email,
+            address: Address,
+            totalAmount: req.body.totalAmount
+        };
+        orderHistorys.push(orderHistory);
+        fs.writeFileSync(config.dataPath + '/orderHistory.json', JSON.stringify(orderHistorys), "utf8");
+        response = {
+            success: "Order History Created"
+        };
+    } else {
+        response = {
+            error: "Please login before checkout"
+        };
+    }
+
+    res.end(JSON.stringify(response));
+});
+
+router.get('/orderHistory', function (req, res) {
+    var config = require('config');
+    var json = fs.readFileSync(config.dataPath + '/orderHistory.json', 'utf8').trim();
+    var orderHistory = JSON.parse(json);
+    var orderHistoryDisplay = [];
+    for(var i = 0; i < orderHistory.length; i++){
+        if(orderHistory[i].username == req.session.loginAs){
+            orderHistoryDisplay.push(orderHistory[i]);
+        } 
+    }
+    res.end(JSON.stringify(orderHistoryDisplay));
+});
+
+module.exports = router;
